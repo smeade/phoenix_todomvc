661e8cd8be66c7a4f429f57eee28f64d06012903
Scott Meade <scott@synapsoftware.com>
2019-03-14 18:59:25 -0600

    Style completed items and show count of items left in footer

--- lib/todos_web/views/todo_view.ex
+++ lib/todos_web/views/todo_view.ex
@@ -1,3 +1,11 @@
 defmodule TodosWeb.TodoView do
   use TodosWeb, :view
+
+  def checked(todo) do
+    if (todo.completed), do: "checked"
+  end
+
+  def completed_class(todo) do
+    if (todo.completed), do: "completed"
+  end
 end
